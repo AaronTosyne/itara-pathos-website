2236ceee8fbe5fb1e96992284dbd00561e2d262a
aaron <abiolatosin3@gmail.com>
2026-02-20 10:29:56 +0100

    Updating CORS with vercel URL

--- server/server.js
+++ server/server.js
@@ -16,8 +16,9 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   process.env.CLIENT_URL,
-  'itara-pathos-website-client-uie4-ffr74vqbl.vercel.app',
-  'https://itarapathos.com', 
+  'https://itara-pathos-website-client-uie4.vercel.app',
+  'https://itara-pathos-website-client-uie4-ffr74vqbl.vercel.app',
+  'https://itarapathos.com',
   'https://www.itarapathos.com'
 ].filter(Boolean); // Remove undefined values
 
